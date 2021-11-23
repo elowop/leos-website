237df36424199efa09d0cc08dedc3b2da9ce911a
Diyar Yasin <47333278+Diyar-Yasin@users.noreply.github.com>
2021-11-22 22:42:13 -0500

    Home page + Basic Side navbar start
    Note that I added react-router-dom trying to use the Link feature

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar.js'
+import Logo from './leo.svg'
+const Home = () => {
+
+    return ( 
+        <div>
+            <img src={Logo}/>
+            <Navbar />
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+const Navbar = ( props ) => {
+    return ( 
+        <div className="Navbar">
+            <a href="#section">WHO WE ARE</a>
+            <a href="#section">INITIATIVES</a>
+            <a href="#section">CALANDAR</a>
+            <a href="#section">CONTACT US</a>
+            <a href="#section">JOIN US</a>
+        </div>
+     );
+}
+ 
+export default Navbar;
