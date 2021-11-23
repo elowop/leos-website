494d78fa5217760997fb1580f8736c14087d19bd
elowop <42976129+elowop@users.noreply.github.com>
2021-11-22 21:29:04 -0500

    setup
    setup/config stuff for mongo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello this is a change
+          Learn React
         </a>
       </header>
     </div>
